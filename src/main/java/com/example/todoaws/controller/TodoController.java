acd9e38a785f561b978062c6d530cd0bee0bfa01
jiyoung-B <catgirl0313@naver.com>
2023-07-12 16:39:24 +0900

    todo앱 백엔드 CRUD

--- src/main/java/com/example/todoaws/controller/TodoController.java
+++ src/main/java/com/example/todoaws/controller/TodoController.java
@@ -6,10 +6,7 @@ import com.example.todoaws.model.TodoEntity;
 import com.example.todoaws.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,6 +48,47 @@ public class TodoController {
         List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
         ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
         return ResponseEntity.ok().body(response);
+
+    }
+
+    @PutMapping("/todo")
+    public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
+            String temporaryUserId = "temporary-user";
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.update(entity);
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+
+
+    }
+
+    @DeleteMapping("/todo")
+    public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user";
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.delete(entity);
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+
+        }
+
 
     }
 }
--- src/main/java/com/example/todoaws/service/TodoService.java
+++ src/main/java/com/example/todoaws/service/TodoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -27,9 +28,40 @@ public class TodoService {
     }
 
     public List<TodoEntity> retrieve(final String userId) {
-
         return repository.findByUserId(userId);
     }
+
+    public List<TodoEntity> update(final TodoEntity entity) {
+        //Validations
+        validate(entity);
+
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+        original.ifPresent(todo -> {
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+            repository.save(entity);
+        });
+
+        return retrieve(entity.getUserId());
+
+    }
+
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        //Validations
+        validate(entity);
+        try {
+            repository.delete(entity);
+
+        } catch (Exception e) {
+            log.error("error deleting entity", entity.getId(), e);
+            throw new RuntimeException("error deleting entity" + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
+
+    }
+
+
 
     public void validate(final TodoEntity entity) {
 
